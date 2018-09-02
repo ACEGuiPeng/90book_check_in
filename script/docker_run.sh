eded0992c2b58d87dd1917f4745ddb1febaa5609
guipeng <aceguipeng@gmail.com>
2018-09-02 18:40:55 +0800

    [feature] add docker script

--- script/docker_run.sh
+++ script/docker_run.sh
@@ -8,5 +8,5 @@ DOCKER_IMAGE_NAME=${DOCKER_CONTAINER_NAME}:v1
 echo "project path is $PROJECT_PATH,start to run..."
 sudo docker stop ${DOCKER_CONTAINER_NAME}
 sudo docker rm ${DOCKER_CONTAINER_NAME}
-sudo docker run --name ${DOCKER_CONTAINER_NAME} -d -e "PYTHONPATH=$DOCKER_PROJECT_PATH"   --log-opt max-size=50m --log-opt max-file=5 ${DOCKER_IMAGE_NAME}
+sudo docker run --name ${DOCKER_CONTAINER_NAME} -d -e "PYTHONPATH=$DOCKER_PROJECT_PATH"  --restart=always --log-opt max-size=50m --log-opt max-file=5 ${DOCKER_IMAGE_NAME}
 sudo docker logs -f ${DOCKER_CONTAINER_NAME}
